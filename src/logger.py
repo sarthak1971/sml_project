f3f62642168ff99ee934fd2e739927fd6c2ce704
Sarthak Somani <sarthaksomani01@gmail.com>
2026-05-20 21:45:10 +0530

    committed

---
+++ src/logger.py
@@ -0,0 +1,13 @@
+import os 
+import logging 
+from datetime import datetime 
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log" 
+log_file = os.path.join(os.getcwd(), LOG_FILE) 
+os.makedirs(os.path.dirname(log_file), exist_ok=True) 
+
+logging.basicConfig( filename=log_file, encoding='utf-8', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s' ) 
+
+logging.debug("Detailed diagnostic information.") 
+logging.info("Confirmation that things are working.") 
+logging.warning("An unexpected issue occurred.")
